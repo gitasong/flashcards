412f4e820b893ac59876e2ec0a06c4782e065229
Nicole Freed and Rena Rallis <rena@>
2017-05-31 13:26:52 -0700

    add JQuery to flip cards; 2 bugs unfixed

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+$("document").ready(function(){
+  $("#javscript .btn").click(function() {
+    $("#javscript .card-front").toggle();
+    $("#javscript .card-back").toggle();
+  });
+
+  $("#operators .btn").click(function() {
+    $("#operators .card-front").toggle();
+    $("#operators .card-back").toggle();
+  });
+
+  $("#variabls .btn").click(function() {
+    $("#variabls .card-front").toggle();
+    $("#variabls .card-back").toggle();
+  });
+
+  $("#naming .btn").click(function() {
+    $("#naming .card-front").toggle();
+    $("#naming .card-back").toggle();
+  });
+
+  $("#functins .btn").click(function() {
+    $("#functins .card-front").toggle();
+    $("#functins .card-back").toggle();
+  });
+
+  $("#meths .btn").click(function() {
+    $("#meths .card-front").toggle();
+    $("#meths .card-back").toggle();
+  });
+
+  $("#args .btn").click(function() {
+    $("#args .card-front").toggle();
+    $("#args .card-back").toggle();
+  });
+
+  $("#params .btn").click(function() {
+    $("#params .card-front").toggle();
+    $("#params .card-back").toggle();
+  });
+
+  $("#retrn .btn").click(function() {
+    $("#retrn .card-front").toggle();
+    $("#retrn .card-back").toggle();
+  });
+
+  $("#chaining .btn").click(function() {
+    $("#chaining .card-front").toggle();
+    $("#chaining .card-back").toggle();
+  });
+
+  $("#strngs .btn").click(function() {
+    $("#strngs .card-front").toggle();
+    $("#strngs .card-back").toggle();
+  });
+
+  $("#bools .btn").click(function() {
+    $("#bools .card-front").toggle();
+    $("#bools .card-back").toggle();
+  });
+
+  $("#undfned .btn").click(function() {
+    $("#undfned .card-front").toggle();
+    $("#undfned .card-back").toggle();
+  });
+
+  $("#non .btn").click(function() {
+    $("#non .card-front").toggle();
+    $("#non .card-back").toggle();
+  });
+
+  $("#escp .btn").click(function() {
+    $("#escp .card-front").toggle();
+    $("#escp .card-back").toggle();
+  });
+
+  $("#alrt .btn").click(function() {
+    $("#alrt .card-front").toggle();
+    $("#alrt .card-back").toggle();
+  });
+
+  $("#cmmts .btn").click(function() {
+    $("#cmmts .card-front").toggle();
+    $("#cmmts .card-back").toggle();
+  });
+
+  $("#jqry .btn").click(function() {
+    $("#jqry .card-front").toggle();
+    $("#jqry .card-back").toggle();
+  });
+
+  $("#attrbs .btn").click(function() {
+    $("#attrbs .card-front").toggle();
+    $("#attrbs .card-back").toggle();
+  });
+});
